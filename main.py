8e659e1de6f7bfb24514095fbcd78ef9b6fa3414
Dubious-Food <255582089+Dubious-Food@users.noreply.github.com>
2026-03-03 18:01:10 -0600

    I swear I'll finish this later

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import argparse
 import sys
 import os
 import json
+TASKS_FILE = "tasks.json"
+def load_tasks():
+    if not os.path.exists(TASKS_FILE):
+        return []
+    with open(TASKS_FILE, "r") as file:
+        return json.load(file)
+
+def save_task(tasks):
+    with open(TASKS_FILE, "w") as file:
+        json.dump(tasks, file, indent=2)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("task", type=str, nargs="?", help="Task to add")
@@ -13,3 +23,6 @@ if len(sys.argv) == 1:
     
 if args.task:
     print(f"Task {args.task} added with ID of 1")
+
+    if args.task:
+        
